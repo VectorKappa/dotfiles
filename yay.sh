37f9d69162d50813f19ac2ba636621f04aa6b9ab
VectorKappa <piotrpatalong@gmail.com>
2021-07-16 23:05:01 +0200

    Added some scripts I use, Optimized keybinds and other settings, Added a script to install my personaly preferred apps via yay

---
+++ yay.sh
@@ -0,0 +1,69 @@
+#!/bin/zsh
+yay -S \
+	zsh \
+	stow \
+	git \
+	polybar \
+	dunst \
+	rofi \
+	bspwm \
+	sxhkd \
+	picom \
+  bat \
+	exa \
+	vim \
+	ranger \
+	alacritty \
+	alsa-utils \
+	thefuck \
+	nerd-fonts-complete \
+	polkit-dumb-agent-git \
+	kvantum-qt5 \
+	lxappearance \
+	nordic-darker-theme\
+	papirus-folders-nordic \
+	capitaine-cursors\
+	noto-fonts-emoji \
+	noto-fonts-cjk \
+	noto-fonts-extra \
+	noto-fonts \
+	nodejs \
+	packagekit \
+	kvantum-theme-nordic-git \
+	atool \
+	ffmpeg \
+	tigervnc \
+	xlunch \
+	rofi-calc \
+	rofi-emoji \
+	rofimoji \
+	cockpit \
+	cockpit-pcp \
+	cockpit-machines \
+	xss-lock \
+	qemu \
+	ebtables \
+	radvd \
+	dmidecode \
+	lsof \
+	strace \
+	dockd \
+	xclip \
+	xsel \
+	xdotool \
+	ffmpegthumbnailer \
+	highlight \
+	libcaca \
+	mediainfo \
+	transmission-cli \
+	transmission-qt \
+	ueberzug \
+	w3m \
+	poppler \
+	odt2txt \
+	gnome-keyring \
+	i3lock-fancy-dualmonitors-git \
+	lxrandr \
+	feh
+
+
